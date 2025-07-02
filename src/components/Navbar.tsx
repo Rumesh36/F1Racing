06bfba99e493fe9e3e9ab38bf17e7b8a135df807
Rumesh36 <rrumesh54@email.com>
2025-07-02 06:43:53 +0530

    error fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,11 @@ const Navbar :React.FC = () => {
     
     <nav>
       <div className='bg-white py-4 flex justify-between '>
-         <div className="text-4xl ml-3 font-extrabold tracking-[0.2rem] text-red-600  max-md:text-2xl  ">
+         <div className="text-4xl ml-3 font-extrabold  text-red-600  max-md:text-2xl  ">
              F1-Racing
          </div>
 
-         <ul className="flex list-none text-xl mr-5 items-center gap-10 max-md:gap-3 font-semibold max-md:text-lg ">
+         <ul className="flex list-none text-xl mr-2 items-center gap-10 max-md:gap-3 font-semibold max-md:text-lg ">
           
           <Link to="/">
           <li>Home</li>
